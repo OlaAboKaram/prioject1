e6074e084a1d971236bf2b2bbd9edf70bcfa0412
OlaAboKaram <106280049+OlaAboKaram@users.noreply.github.com>
2022-07-05 13:17:38 -0700

    Add files via upload

---
+++ app/Models/trip.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class trip extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'age',
+        'type',
+        'price',
+        'start_date',
+        'expiry_date',
+        'start_trip',
+        'end_trip',
+        'rest',
+        'total',
+        'image',
+        'reiteration',
+        'provinces',
+        'coutinent',
+    ];
+}
